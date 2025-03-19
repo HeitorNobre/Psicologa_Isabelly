8a969e0de973d881af3113fa0bba7c41816e3ced
HeitorNobre <heitordev01@gmail.com>
2025-03-19 11:51:21 -0300

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,7 +14,7 @@ const Services = () => {
         <div className="service-item">
         <img src={adultoPresencial} alt="Atendimento adulto presencial" />
           <h3>Atendimento adulto presencial</h3>
-          <p>R$ 230</p>
+          <p>R$ 215</p>
           <p>Oferecemos atendimento psicológico presencial em nosso espaço no Tatuapé, SP. Com um local acolhedor e confidencial, estamos prontos para ajudá-lo a superar desafios emocionais e melhorar seu bem-estar.<br/><br/>
             Agende sua sessão e comece sua jornada de autoconhecimento e transformação!
           </p>
@@ -28,7 +28,7 @@ const Services = () => {
         <div className="service-item">
           <img src={infantilPresencial} alt="Atendimento infantil presencial" />
           <h3>Atendimento infantil presencial</h3>
-          <p>R$ 230</p>
+          <p>R$ 215</p>
           <p>Oferecemos atendimento psicológico presencial para crianças em nossa clínica no Tatuapé, SP. Em um ambiente acolhedor, ajudamos os pequenos a expressar emoções, desenvolver habilidades sociais e superar desafios.
             <br /><br />
             Agende uma sessão e proporcione ao seu filho um espaço seguro para crescer e se desenvolver!</p>
@@ -42,7 +42,7 @@ const Services = () => {
         <div className="service-item">
           <img src={onlineGeral} alt="Atendimento online" />
           <h3>Atendimento online</h3>
-          <p>R$ 200</p>
+          <p>R$ 170</p>
           <p>Oferecemos sessões de terapia online com a TCC, no conforto da sua casa. Em um ambiente virtual seguro, você pode acessar apoio emocional e desenvolver estratégias para enfrentar desafios diretamente do conforto do seu lar.
             <br /><br />
             Agende sua sessão online e dê o primeiro passo rumo ao bem-estar!</p>
